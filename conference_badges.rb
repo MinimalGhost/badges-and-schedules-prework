999228e119adc036e25718c5502f5f35f1dd69bf
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 14:28:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each_line do |attendees|
+  batch_badge_creator.each_line do |element|
     puts attendees
   end
 end
