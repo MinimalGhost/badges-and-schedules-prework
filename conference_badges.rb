e5438d38cc2ace8783275ad0169f093d962a8bbf
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 13:56:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,7 @@
 def badge_maker(name)
   badgeArr = []
-  badgeArr.push("Hello, my name is #{name}") 
+  badgeArr.push("Hello, my name is #{name}")
+  return badgeArr[0] 
 end
 
 def batch_badge_creator(attendees)
