2916c9999cf589d861027ac948f63ca8b3a3117d
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 14:12:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  batchBadger = attendees.map { |name| "Hello, my name is #{name}." }
+  batchBadger = attendees.map { |name| 
+    "Hello, my name is #{name}." 
+  }
   return batchBadger
+end
+
+def assign_rooms(attendees)
+  batchAssigner = attendees.each_with_index { |name, index| 
+    "Hello, #{name}! You'll be assigned to room #{index}"
+  }
 end
